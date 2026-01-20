77a7c2da5af011cda2cd9221bde33679cffc3f6d
sy-muskhan <symuskan786@gmail.com>
2026-01-20 11:14:16 +0530

    DAY_10

---
+++ UnitTesting_CalculatorApp/CalculatorTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using CalculatorLibrary;
+using System;
+
+namespace DAY_9
+{
+    class CalculatorTests
+    {
+        Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [Test]
+        public void Add_TwoNumbers_ReturnsSum()
+        {
+            int result = calculator.Add(2, 3);
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Subtract_TwoNumbers_ReturnsDifference()
+        {
+            int result = calculator.Subtract(5, 3);
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Multiply_TwoNumbers_ReturnsProduct()
+        {
+            int result = calculator.Multiply(2, 3);
+            Assert.That(result, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Divide_TwoNumbers_ReturnsQuotient()
+        {
+            int result = calculator.Divide(10, 2);
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Divide_ByZero_ThrowsException()
+        {
+            Assert.Throws<DivideByZeroException>(() => calculator.Divide(10, 0));
+        }
+
+
+    }
+}
