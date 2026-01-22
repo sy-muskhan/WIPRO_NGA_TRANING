7168156f119ada43235900bdee97afc85404b532
SY Muskhan <137699581+sy-muskhan@users.noreply.github.com>
2026-01-22 20:19:09 +0530

    Update

--- DAY_13 SQL.sql
+++ DAY_13 SQL.sql
@@ -1,75 +1,191 @@
-use NGATraining;
+Create Database NGATraining;
 Go
---WHY WE NEED JOINS
---Suppose if we have 3 different tables
--- Courses( Master table )
--- Students ( transaction Table)
--- Trainers( Self Join use cases)
 
-Create table MyCourses(
-	CourseId int primary key,
-	StudentName varchar(100),
-);
+Use NGATraining;
+Go
 
-CREATE TABLE My_Students (
+-- Create table
+CREATE TABLE Students
+(
     StudentId INT PRIMARY KEY,
-    StudentName VARCHAR(50),
+    Email VARCHAR(50),
+    Age INT,
     CourseId INT
 );
-CREATE TABLE Trainers (
-    TrainerId INT PRIMARY KEY,
-    TrainerName VARCHAR(50),
-    ManagerId INT
+
+-- Insert records
+INSERT INTO Students VALUES (1, 'rahul@gmail.com', 20, 101);
+INSERT INTO Students VALUES (2, 'neha@gmail.com', 22, 102);
+INSERT INTO Students VALUES (3, 'amit@gmail.com', 19, 101);
+INSERT INTO Students VALUES (4, 'amitabh@gmail.com', 21, 101);
+
+-- Display all records
+SELECT * FROM Students;
+
+
+--Counting
+SELECT COUNT(*) AS TotalStudents
+FROM Students;
+
+--Average
+SELECT AVG(Age) AS AverageAge
+FROM Students;
+
+--Scalar Functions
+SELECT 
+    StudentId,
+    LEN(Email) AS EmailLength,
+    GETDATE() AS CurrentDate
+FROM Students;
+
+
+--Grouping
+SELECT 
+    CourseId,
+    COUNT(*) AS StudentCount
+FROM Students
+GROUP BY CourseId;
+
+--Transactions: Commit, RollBack,Save Point
+--ROLLBACK
+BEGIN TRANSACTION;
+    UPDATE Students
+    SET Age = Age + 1
+    Where CourseId = 101;
+
+ROLLBACK; -- Above changes are not reflected hence rollbacked to previous consistent state
+--No change Due to ROllBack
+
+--COMMIT
+BEGIN TRANSACTION;
+    UPDATE Students
+    SET Age = Age + 1
+    Where CourseId = 101;
+COMMIT;
+
+--SAVEPOINT
+--All the changes will be saved till this savepoint
+BEGIN TRANSACTION;
+UPDATE students Set Age = 25 Where StudentId = 2;
+SAVE Transaction s1; 
+--Above operations are now permanent and its state is saved as S1
+
+
+--Below Update is roll backed and previous state that is S1 is achieved
+UPDATE Students Set Age = 30 where StudentId = 1;
+RollBack Transaction s1;
+Commit;
+
+--ACCESS CONTROL: Grant & Revoke
+
+--Grant: Here User1 is allowed to perform these operations.
+Grant select, Insert on students to User1;
+--Revoke: Here Revoking user1 to perform insert Operations.
+Revoke Insert on Students From User1;
+
+--DDL with Alter:
+Alter table Students
+Add Phone varchar(50);
+
+Select * from Students; 
+--Q: What can be done if we dont want to write select command again and again
+--Ans: Creating a function that displays/run select * from table
+Create Function fn_DisplayStudentsTable()
+returns Table
+As return
+(
+Select StudentID, Email, Age, CourseId from students
 );
 
-INSERT INTO MyCourses VALUES
-(101, 'Full Stack'),
-(102, 'Python'),
-(103, 'Java');
+Select * from fn_DisplayStudentsTable();
 
 
-INSERT INTO My_Students VALUES
-(1, 'Rahul', 101),
-(2, 'Neha', 102),
-(3, 'Amit', 101),
-(4, 'Sonal', NULL);
+--A function with Parameters
+Create Function dbo.GetStudentByCourseId(@CourseId int)
+Returns Table
+As
+Return 
+(
+Select studentId, Email, Age from Students
+where CourseId = @CourseId
+);
 
-INSERT INTO Trainers VALUES
-(1, 'Arjun', NULL),
-(2, 'Ravi', 1),
-(3, 'Sneha', 1),
-(4, 'Kiran', 2);
+Select * from fn_DisplayStudentsTable();
+Select * from dbo.GetStudentByCourseId(101);
+
+--Foreign Key Implementation
+--This Enforces a logic that there can not be student without values of course
+--Which means first we have to insert values for courses table then we will insert students value in wipro university
 
 
---Implementing Joins
---Types of joins
---1.Inner Join-It will return commnon values in both the tables
+--Parent Table
+Create Table Courses(
+    CourseId int primary Key,
+    CourseName varchar(100) not null,
+    DurationInMonths int check(DurationInMonths >0)
 
-Select s.My_Students, c.MyCourses
-from My_Students s
-Inner Join MyCourses c
-on s.CourseId = c.CourseID;
+);
 
---Left Join: Will re
+--This Enforces a logic that there can not be student without values of course
+--Which means first we have to insert values for courses table then we will insert students value in wipro university
+--So we are inserting courses here first
 
---Right Join : All right record + Matching Record
---It will display all courses even if there is no enrolled student
 
-Select s.StudentName, s.StudentId, c.CourseName, c.CourseId
-from My_Students s
-Right Join MyCourses c
-on s.CourseId = c.CourseId;
+Insert into Courses values (101, 'Full Stack Java Development', 6);
+Insert into Courses values (102, 'Data Analytics', 10);
+Insert into Courses values(103, 'Cloud Engineering', 19);
 
---Full Outer Join
---It returns all the records from both table
---Complete Enrollement Audit
+Select * from courses;
 
-Select s.StudentName, s.StudentId,c.CourseName, c.CourseId
-From My_Students s
-Full Outer Join MyCourses
-on s.CourseId = c.CourseId;
+--This is the child Table
+Create Table WiproUniversity
+(
+StudentId INT PRIMARY KEY,
+    Email VARCHAR(100) Unique,
+    Age INT check (Age >= 18),
+    CourseId INT,
+    Constraint fk_Students_Courses
+    Foreign Key (CourseId)
+    References Courses(CourseId)
+);
 
---Self Join
---Table Joins to itself
+--after initializing courses table now we have to initialize this table values
 
---Cross Join
+INSERT INTO WiproUniversity VALUES (1, 'rahul@gmail.com', 20, 101);
+INSERT INTO WiproUniversity VALUES (2, 'neha@gmail.com', 22, 102);
+INSERT INTO WiproUniversity VALUES (3, 'amit@gmail.com', 19, 103);
+
+Select * from WiproUniversity;
+
+--The above line will give you error because the courseId in the line is 109 which is not present in the courses table
+INSERT INTO WiproUniversity VALUES (3, 'amit@gmail.com', 19, 109);
+--Output: This CourseId is not in the courses table
+
+--Here Usage of delete will also give you error because if there is atleast one value of courseId present in the WiprUniversity table there is no chance to delete the data.
+Delete from Courses where courseId = 101;
+
+--If you really want to delete use CASCADE TABLE by dropping the tables
+Drop table WiproUniversity;
+
+Create Table WiproUniversity
+(
+StudentId INT PRIMARY KEY,
+    Email VARCHAR(100) Unique,
+    Age INT check (Age >= 18),
+    CourseId INT,
+    Constraint fk_Students_Courses
+    Foreign Key (CourseId)
+    References Courses(CourseId)
+    On delete CASCADE
+);
+--Now we can delete parent column so that the child column will be automatically deleted.
+
+INSERT INTO WiproUniversity VALUES (1, 'rahul@gmail.com', 20, 101);
+INSERT INTO WiproUniversity VALUES (2, 'neha@gmail.com', 22, 102);
+INSERT INTO WiproUniversity VALUES (3, 'amit@gmail.com', 19, 103);
+INSERT INTO WiproUniversity VALUES (4, 'muski@gmail.com', 27, 101);
+
+Select * from WiproUniversity;
+delete from WiproUniversity where CourseId = 101;
+
+--Now row 1 and row 4 will be deleted because we used cascade deleting here. So courseId with 101 will be deleted.
